5ce14a0a6d984de8e3b40251cd14f7dec83a5cf9
Marcos Vieira <marcosvieira482@gmail.com>
2023-07-07 19:57:22 -0300

    feat: download buttons added

--- script.js
+++ script.js
@@ -6,21 +6,24 @@ const qrImg = document.querySelector(".container .qr-code img")
 const corFundo = document.querySelector(".container .corFundo")
 const corLinha = document.querySelector(".container .corLinha")
 
+const downloadPNG = document.querySelector(".download .btn-png")
+const downloadSVG = document.querySelector(".download .btn-svg")
+
 let preValue;
 let size = "150x150";
-let bgcolor, color
+let bgcolor, color, imgSVG, imgPNG
 
 corFundo.addEventListener("input", () => {
-	bgcolor = (corFundo.value).replace('#', '');
+    bgcolor = (corFundo.value).replace('#', '');
 });
 
 corLinha.addEventListener("input", () => {
-	color = (corLinha.value).replace('#', '');
+    color = (corLinha.value).replace('#', '');
 });
 
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value.trim();
-    if (!qrImg || preValue === qrValue) {
+    if (!qrImg || qrValue === "") {
         return
     }
     preValue = qrValue;
@@ -29,7 +32,34 @@ generateBtn.addEventListener("click", () => {
     qrImg.addEventListener("load", () => {
         container.style.height = '600px'
         generateBtn.innerText = "Gerar QR Code"
+        imgSVG = qrImg.src
+        imgPNG = (qrImg.src).replace('svg', 'png')
     })
+
+})
+
+async function downloadImage(imageSrc, imageName) {
+    const response = await fetch(imageSrc);
+    const blobImage = await response.blob();
+
+    const href = URL.createObjectURL(blobImage);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = href;
+    anchorElement.download = imageName;
+
+    document.body.appendChild(anchorElement);
+    anchorElement.click();
+
+    document.body.removeChild(anchorElement);
+    window.URL.revokeObjectURL(href);
+}
+
+downloadPNG.addEventListener("click", () => {
+    downloadImage(imgPNG, 'meu-qrcode.png',);
+})
+
+downloadSVG.addEventListener("click", () => {
+    downloadImage(imgSVG, 'meu-qrcode.svg',);
 })
 
 qrInput.addEventListener("keyup", () => {
@@ -39,6 +69,6 @@ qrInput.addEventListener("keyup", () => {
 })
 
 window.onload = () => {
-	corFundo.value  = "#FFFFFF";
-	corLinha.value  = "#000000";
+    corFundo.value = "#FFFFFF";
+    corLinha.value = "#000000";
 };
